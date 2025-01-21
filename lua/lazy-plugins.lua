d201c4d735ea241649a9de7c831492bdc5122bae
pravinboopathy <pravinboopathy@gmail.com>
2025-01-21 13:55:25 -0600

    disabled gitsigns, opting to use lazy git for now

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -23,7 +23,7 @@ require('lazy').setup({
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
-  require 'kickstart/plugins/gitsigns',
+  --  require 'kickstart/plugins/gitsigns',
 
   require 'kickstart/plugins/which-key',
 
