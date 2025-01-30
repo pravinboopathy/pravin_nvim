93a6f82e1109a655e7c8a4b9976822b0f3d88c14
pravinboopathy <pravinboopathy@gmail.com>
2025-01-29 18:43:16 -0600

    add copilot chat and inline

---
+++ lua/custom/plugins/codecompanion.lua
@@ -0,0 +1,32 @@
+return {
+  'olimorris/codecompanion.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  config = function()
+    require('codecompanion').setup {
+      strategies = {
+        chat = {
+          adapter = 'copilot',
+        },
+        inline = {
+          adapter = 'copilot',
+        },
+        agent = {
+          adapter = 'copilot',
+        },
+      },
+    }
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'codecompanion',
+      callback = function()
+        vim.defer_fn(function()
+          vim.api.nvim_buf_set_keymap(0, 'i', '<C-c>', '<Esc>', { noremap = true, silent = true })
+        end, 50) -- Delay for 50ms to allow the filetype to load
+      end,
+    })
+    vim.keymap.set('n', '<leader>cc', ':CodeCompanionChat<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>ci', ':CodeCompanion<CR>', { noremap = true, silent = true })
+  end,
+}
