ec795a1516aef0e1e624fcc0f4cd08412efaab43
pravinboopathy <pravinboopathy@gmail.com>
2025-03-12 10:18:09 -0500

    disable tmux navigator

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -61,6 +61,9 @@ require('lazy').setup({
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
+  --
+  -- List of disabled plugins
+  { 'vim-tmux-navigator', enabled = false },
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins' },
   --
