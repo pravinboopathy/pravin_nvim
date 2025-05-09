25520b326f405d578d7cc1ee79d2f94250adbc3b
pravinboopathy <pravinboopathy@gmail.com>
2025-05-09 14:58:22 -0500

    show hidden files, but ignore git related stuff

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -79,6 +79,22 @@ return {
             require('telescope.themes').get_dropdown(),
           },
         },
+        pickers = {
+          find_files = {
+            hidden = true,
+            find_command = {
+              'rg',
+              '--files',
+              '--hidden',
+              '--glob',
+              '!.git/*',
+              '--glob',
+              '!.gitignore',
+              '--glob',
+              '!.github/*',
+            },
+          },
+        },
       }
 
       -- Enable Telescope extensions if they are installed
