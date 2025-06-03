bc7101a0d1d8c88e6980dd2b437ff99006377e0f
pravinboopathy <pravinboopathy@gmail.com>
2025-06-03 09:47:56 -0500

    justin's agent_prompt

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -1,3 +1,12 @@
+local agent_prompt = [[
+1. When I ask to build a new feature, always provide a plan and pause for me to confirm.
+2. When implementing a new multi-step plan, always validate unit tests and commit after each step.
+3. When using Go, `make test` will run unit tests.
+4. When using TypeScript, `yarn test` will run unit tests.
+5. Prefer not to duplicate code and instead refactor if possible.
+6. Prefer separating logically distinct code into different files.
+]]
+
 return {
   'olimorris/codecompanion.nvim',
   dependencies = {
@@ -17,23 +26,21 @@ return {
             },
           })
         end,
-        copilot_chat = function()
-          return require('codecompanion.adapters').extend('copilot', {
-            name = 'copilot',
-            schema = {
-              model = {
-                default = 'gpt-4.1',
-              },
-            },
-          })
-        end,
       },
       strategies = {
         chat = {
-          adapter = 'copilot_chat',
+          adapter = 'copilot',
+          tools = {
+            groups = {
+              ['full_stack_dev'] = {
+                -- overrides agent system_prompt
+                system_prompt = agent_prompt,
+              },
+            },
+          },
         },
         inline = {
-          adapter = 'copilot_chat',
+          adapter = 'copilot',
         },
         agent = {
           adapter = 'copilot_agent',
