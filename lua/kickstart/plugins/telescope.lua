c53fdfc52988f801673423f2130c19afd3339573
pravinboopathy <pravinboopathy@gmail.com>
2025-06-12 15:34:32 -0500

    s/ for grep across / for grep buffer, + worktree sgl and sgc
    cleanup

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -114,18 +114,18 @@ return {
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>s/', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       local extensions = require('telescope').extensions
-      vim.keymap.set('n', '<leader>stl', extensions.git_worktree.git_worktree, { desc = '[S] Work[T]rees [L]ist' })
+      vim.keymap.set('n', '<leader>sgl', extensions.git_worktree.git_worktree, { desc = '[S] Work[T]rees [L]ist' })
       -- <Enter> - switches to that wrktree
+      vim.keymap.set('n', '<leader>sgc', extensions.git_worktree.create_git_worktree, { desc = '[S] Work[T]rees [C]reate' })
       -- <c-d> - deletes that worktree
       -- <c-f> - toggles forcing of the next deletion
-      vim.keymap.set('n', '<leader>stc', extensions.git_worktree.create_git_worktree, { desc = '[S] Work[T]rees [C]reate' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
