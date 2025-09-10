a438ee0c338f0add2e8027211372c938e5e73eb6
pravinboopathy <pravinboopathy@gmail.com>
2025-09-10 15:30:34 -0500

    neorg export to md

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -11,12 +11,19 @@ return {
           config = {
             workspaces = {
               notes = '~/notes',
+              project_notes = '~/project_notes',
             },
             default_workspace = 'notes',
           },
         },
         ['core.summary'] = {},
         ['core.integrations.nvim-cmp'] = {},
+        ['core.export'] = {},
+        ['core.export.markdown'] = {
+          config = {
+            extensions = 'all',
+          },
+        },
       },
     }
     vim.wo.foldlevel = 99
